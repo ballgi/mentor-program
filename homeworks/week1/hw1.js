33d62a62738720b17c2d08748d7d09307030e68b
ballgi <chihung.0410@gmail.com>
2018-07-12 08:37:52 +0800

    Update hw1.js

--- homeworks/week1/hw1.js
+++ homeworks/week1/hw1.js
@@ -1,3 +1,11 @@
-export const stars = (n) => {  
-  return ;
+export const stars = (n) => {
+  var aa = [];
+  for (var i =1;i<=n;i++) {
+    var bb = '';
+    for (var ii =1;ii<=n;ii++) {
+      bb += '*';
+    }
+    aa.push(bb);
+  }
+  return aa;
 }
