dd9c9e1ad216e61e60a883446a77be8c12e5f576
ballgi <chihung.0410@gmail.com>
2018-07-12 08:54:36 +0800

    Update hw1.test.js

--- homeworks/week1/hw1.test.js
+++ homeworks/week1/hw1.test.js
@@ -4,4 +4,16 @@ describe("hw1", () => {
   it("should return correct answer when n = 1", () => {
     expect(stars(1)).toEqual(['*'])
   })
+  
+   it("should return correct answer when n = 2", () => {
+    expect(stars(2)).toEqual(['*','**'])
+  })
+  
+   it("should return correct answer when n = 4", () => {
+    expect(stars(4)).toEqual(['*','**','***','****'])
+  })
+  
+  it("should return correct answer when n = 6", () => {
+    expect(stars(6)).toEqual(['*','**','***','****','*****','******'])
+  })
 })
