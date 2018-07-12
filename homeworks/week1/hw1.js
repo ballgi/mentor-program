b3b0bc435062c745d60b263e8fb38df7be0f1a16
ballgi <chihung.0410@gmail.com>
2018-07-12 08:37:07 +0800

    Update hw1.js

--- homeworks/week1/hw1.js
+++ homeworks/week1/hw1.js
@@ -1,11 +1,3 @@
-export const stars = (n) => {
-  var aa = [];
-  for (var i =1;i<=n;i++) {
-    var bb = '';
-    for (var ii =1;ii<=n;ii++) {
-      bb += '*';
-    }
-    aa.push(bb);
-  }
-  return aa;
+export const stars = (n) => {  
+  return ;
 }
